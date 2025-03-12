bc342c9d6a78cf1d5317957588c0d4067d6f7a96
Alex Matsuev <alex.matsuev@gmail.com>
2025-03-12 18:17:04 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"s0611-23/internal/service"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	api := fiber.New()
+
+	svc := service.New()
+
+	api.Get("/", svc.RootHandler)
+	api.Get("/api/user", svc.UserRead)
+	api.Get("/api/profile", svc.ProfileRead)
+
+	if err := api.Listen(":7080"); err != nil {
+		log.Fatalln(err)
+	}
+}
